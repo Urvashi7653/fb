64ea54bf5c10290f026d3373b3e2a943fc0ee748
Urvashi7653 <urvashi.suden@gmail.com>
2022-07-08 19:18:32 +0530

    part one

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const {validateEmail} = require("../helpers/validation")
+const { validateEmail,validateLength } = require("../helpers/validation");
 exports.register = async (req, res) => {
   try {
     const {
@@ -14,18 +14,38 @@ exports.register = async (req, res) => {
       bDate,
     } = req.body;
 
-if(!validateEmail(email)){
-  return res.status(400).json({
-    message:"Invalid email address"
-  })
-}
+    if (!validateEmail(email)) {
+      return res.status(400).json({
+        message: "Invalid email address",
+      });
+    }
 
-const check = await User.findOne({email:email})
-if(check){
-  return res.status(400).json({
-    message:"Email already in use"
-  })
-}
+    if (!validateLength(first_name,3,30)) {
+      return res.status(400).json({
+        message: "First name should be between 3 and 30 characters",
+      });
+    }
+
+    if (!validateLength(last_name,3,30)) {
+      return res.status(400).json({
+        message: "Last name should be between 3 and 30 characters",
+      });
+    }
+
+    if (!validateLength(password,6,30)) {
+      return res.status(400).json({
+        message: "Password should be between 6 and 30 characters",
+      });
+    }
+   
+    const bcryptedpassword = await bcrypt.hash(password,10)
+
+    const check = await User.findOne({ email: email });
+    if (check) {
+      return res.status(400).json({
+        message: "Email already in use",
+      });
+    }
 
     const user = await new User(req.body);
     user.save();
